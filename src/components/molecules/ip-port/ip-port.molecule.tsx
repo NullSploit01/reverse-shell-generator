8fed4071b6199d50cbf437ea67a747c1242359d0
NullSploit01 <harshaldharmik099@gmail.com>
2023-09-02 23:06:47 +0530

    add basic port validation

--- src/components/molecules/ip-port/ip-port.molecule.tsx
+++ src/components/molecules/ip-port/ip-port.molecule.tsx
@@ -7,10 +7,25 @@ const IPPort = () => {
   const { ip, port, setIP, setPort } = useIPPortContext()
 
   const handleIPChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // validate ip and update
+
+    // const ipv4Pattern = /^(\d{1,3}\.){3}(\d{1,3})$/
+    // const ipAddress = e.target.value
+
+    // if (!ipv4Pattern.test(ipAddress)) {
+    //   return
+    // }
+
     setIP(e.target.value)
   }
 
   const handlePortChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const portToNumber = Number(e.target.value)
+
+    if (portToNumber > 65535 || portToNumber < 0) {
+      return
+    }
+
     setPort(e.target.value)
   }
 
