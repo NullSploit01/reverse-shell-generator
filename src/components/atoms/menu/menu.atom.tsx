4c01cc90a9eb5bb0329b95fb92ef6b8486f81cd6
NullSploit01 <harshaldharmik099@gmail.com>
2023-09-09 20:15:47 +0530

    fix id

--- src/components/atoms/menu/menu.atom.tsx
+++ src/components/atoms/menu/menu.atom.tsx
@@ -1,25 +1,22 @@
 import React from 'react'
 
-import { generateRandomId } from '@/utils/general.util'
-
 import MenuCollapseIcon from './components/menu-collapse-icon.component'
 import MenuItem from './components/menu-item.component'
 import { IMenuProps } from './interface'
 
 const Menu: React.FC<IMenuProps> = ({ label, data, onClick }) => {
   const [collapsed, setCollapsed] = React.useState(false)
-  const id = generateRandomId()
 
   return (
     <section className="menu-section">
       <div className="menu-items">
         <input
           type="checkbox"
-          id={`menu-${id}`}
+          id={`menu`}
           className="menu-toggle"
           onClick={() => setCollapsed(!collapsed)}
         />
-        <label tabIndex={-1} className="menu-item justify-between" htmlFor={`menu-${id}`}>
+        <label tabIndex={-1} className="menu-item justify-between" htmlFor={`menu`}>
           <span>{label}</span>
           <MenuCollapseIcon />
         </label>
