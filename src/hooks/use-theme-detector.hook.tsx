0da7c3c45d11fac0e7f5fbc39a91c7ae42db2af1
NullSploit01 <harshaldharmik099@gmail.com>
2023-09-10 10:41:44 +0530

    fix usethemedetector hook

--- src/hooks/use-theme-detector.hook.tsx
+++ src/hooks/use-theme-detector.hook.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 
 export const useThemeDetector = () => {
-  const isDarkTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches
-
-  const [theme, setTheme] = useState<'light' | 'dark'>(isDarkTheme() ? 'dark' : 'light')
+  const [theme, setTheme] = useState<'light' | 'dark'>('light')
 
   useEffect(() => {
+    const isDarkTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches
+    setTheme(isDarkTheme() ? 'dark' : 'light')
+
     window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', (event) => {
       const newColorScheme = event.matches ? 'light' : 'dark'
       setTheme(newColorScheme)
